213b9a585b1f816b2aad962d0f37c764c1eb8856
Nguyen Tuan Anh <nguyentuananh1993@gmail.com>
2015-02-12 23:42:38 +0000

    ユーザーがサインアップできる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+
   devise_for :users
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
